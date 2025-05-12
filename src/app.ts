56a03fcf5204c332f72ab34f6d9de9ab2519be5d
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-05-12 13:45:15 +0600

    cors link added

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,11 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://basa-finder-client-beta.vercel.app',
+    ],
     credentials: true,
   }),
 );
